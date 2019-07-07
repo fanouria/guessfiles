a5e6aadaf7adc776327667e7f97b251f675f42f4
fanouria <fanouria.ath@gmail.com>
2019-07-07 16:45:03 +0300

    Update guessinggame.sh
    Check if what you entered is an integer

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,26 +4,35 @@
 function isinteger {
 re='^[0-9]+$'
 if ! [[ $1 =~ $re ]] ; then
-   echo "error: Not an integer" >&2
+guessinteger=0
+else
+guessinteger=1
 fi
 }
 
 echo "Try to guess how many files are in the current directory!"
 while true;
 do
-echo "Type your guess and then press Enter:"
-read guess
-isinteger $guess
-nfile=$(ls -l| wc -l)
-if [[ $guess -eq  $nfile ]]
-then
-  echo "Congratulations!!! Your guess is right."
-  break
-elif [[ $guess -gt  $nfile ]]
-then
-  echo "Your guess is too high."
-else
-  echo "Your guess is too low."
-fi
-done
+	echo "Type your guess and then press Enter:"
+	read guess
+	isinteger $guess
+	while [[ $guessinteger = 0 ]] 
+	do
+		echo "Not an integer!Type again your guess and then press Enter:"
+		read guess
+		isinteger $guess
+	done
 
+	nfile=$(ls -l| wc -l)
+	if [[ $guess -eq $nfile ]]
+	then
+		echo "Congratulations!!! Your guess is right."
+	break
+	elif [[ $guess -gt $nfile ]]
+	then
+		echo "Your guess is too high."
+	else
+		echo "Your guess is too low."
+	fi
+done
+	
