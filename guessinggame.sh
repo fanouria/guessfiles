8478117a2e82a5abf2de76b411663ec2708bfea5
fanouria <fanouria.ath@gmail.com>
2019-07-07 10:59:34 +0300

    Update guessinggame.sh
    Added a check if guess is an integer but not a prompt to regive a guess

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
-echo "Type your guess about how many files are in the current directory and then press Enter:"
+function isinteger {
+re='^[0-9]+$'
+if ! [[ $1 =~ $re ]] ; then
+   echo "error: Not an integer" >&2
+fi
+}
+
+echo "Try to guess how many files are in the current directory!"
+while true;
+do
+echo "Type your guess and then press Enter:"
 read guess
+isinteger $guess
 nfile=$(ls -l| wc -l)
-if []
+if [[ $guess -eq  $nfile ]]
+then
+  echo "Congratulations!!! Your guess is right."
+  break
+elif [[ $guess -gt  $nfile ]]
+then
+  echo "Your guess is too high."
+else
+  echo "Your guess is too low."
+fi
+done
+
